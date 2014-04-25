fdb1e8be54425d2ac22692067678046e1c89946d
Ryan Blue <rblue@cloudera.com>
2014-04-25 15:20:49 -0700

    CDK-342: Add descriptor builder methods for JSON partition strategies.
    This adds methods that mirror the schema setting methods in the
    DatasetDescriptor.Builder. In addition, if the partition strategy is not
    explicitly set using those methods, then it is parsed from the schema.
    
    This commit also removes the Schema from the parser. Partition
    strategies can be defined independently, and consistency is checked in
    the DatasetDescriptor. The Schema was only used to set the
    IdentityFieldPartitioner source (and result) class, which is now Object.
    The only place this was used specifically was in valueFromString, which
    now requires the expected type.

--- kite-data/kite-data-core/src/main/java/org/kitesdk/data/filesystem/FileSystemDatasetRepository.java
+++ kite-data/kite-data-core/src/main/java/org/kitesdk/data/filesystem/FileSystemDatasetRepository.java
@@ -44,6 +44,8 @@ import org.apache.avro.Schema;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.kitesdk.data.spi.SchemaUtil;
+import org.kitesdk.data.spi.partition.IdentityFieldPartitioner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -307,7 +309,7 @@ public class FileSystemDatasetRepository extends AbstractDatasetRepository {
    * @return a partition key representing the partition at the given path
    * @since 0.4.0
    */
-  @SuppressWarnings("deprecation")
+  @SuppressWarnings({"unchecked", "deprecation"})
   public static PartitionKey partitionKeyForPath(Dataset dataset, URI partitionPath) {
     Preconditions.checkState(dataset.getDescriptor().isPartitioned(),
         "Attempt to get a partition on a non-partitioned dataset (name:%s)",
@@ -337,6 +339,7 @@ public class FileSystemDatasetRepository extends AbstractDatasetRepository {
           fieldPartitioners.size()));
     }
 
+    Schema schema = dataset.getDescriptor().getSchema();
     List<Object> values = Lists.newArrayList();
     int i = 0;
     for (String part : parts) {
@@ -353,8 +356,14 @@ public class FileSystemDatasetRepository extends AbstractDatasetRepository {
             "'%s' in partition %s.", fieldName, partitionUri));
       }
       String stringValue = split.next();
-      Object value = fp.valueFromString(stringValue);
-      values.add(value);
+
+      if (fp instanceof IdentityFieldPartitioner) {
+        Class<? extends Object> expected = SchemaUtil.TYPE_TO_CLASS.get(
+            schema.getField(fp.getSourceName()).schema().getType());
+        values.add(fp.valueFromString(stringValue, expected));
+      } else {
+        values.add(fp.valueFromString(stringValue, fp.getType()));
+      }
     }
     return org.kitesdk.data.impl.Accessor.getDefault().newPartitionKey(
         values.toArray(new Object[values.size()]));
