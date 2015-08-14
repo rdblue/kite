651e6e2c2e4c695bec7b86e1796609bd13c6029f
Prasanna Rajaperumal <prasanna@cloudera.com>
2015-08-14 13:15:30 -0700

    more formatting

--- kite-data/kite-data-kudu/src/main/java/org/kitesdk/data/kudu/KuduDatasetRepository.java
+++ kite-data/kite-data-kudu/src/main/java/org/kitesdk/data/kudu/KuduDatasetRepository.java
@@ -1,12 +1,12 @@
- /**
+/**
  * Copyright 2015 Cloudera Inc.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 package org.kitesdk.data.kudu;
-import org.kitesdk.data.Dataset;
+
 import org.kitesdk.data.DatasetDescriptor;
 import org.kitesdk.data.RandomAccessDataset;
 import org.kitesdk.data.spi.AbstractDatasetRepository;
@@ -37,20 +37,26 @@ public class KuduDatasetRepository extends AbstractDatasetRepository {
   }
 
   @Override
-  public <E> RandomAccessDataset<E> load(String namespace, String name, Class<E> type) {
-    return new KuduDataset<E>(namespace, name, kuduClient, kuduTable, metadataProvider.load(name), repositoryUri, type);
+  public <E> RandomAccessDataset<E> load(String namespace, String name,
+      Class<E> type) {
+    return new KuduDataset<E>(namespace, name, kuduClient, kuduTable,
+        metadataProvider.load(name), repositoryUri, type);
   }
 
   @Override
-  public <E> RandomAccessDataset<E> create(String namespace, String name, DatasetDescriptor descriptor, Class<E> type) {
+  public <E> RandomAccessDataset<E> create(String namespace, String name,
+      DatasetDescriptor descriptor, Class<E> type) {
     metadataProvider.create(name, descriptor);
-    return new KuduDataset<E>(namespace, name, kuduClient, kuduTable, descriptor, repositoryUri, type);
+    return new KuduDataset<E>(namespace, name, kuduClient, kuduTable,
+        descriptor, repositoryUri, type);
   }
 
   @Override
-  public <E> RandomAccessDataset<E> update(String namespace, String name, DatasetDescriptor descriptor, Class<E> type) {
+  public <E> RandomAccessDataset<E> update(String namespace, String name,
+      DatasetDescriptor descriptor, Class<E> type) {
     // this will throw a not implemented exception
-    return new KuduDataset<E>(namespace, name, kuduClient, kuduTable, metadataProvider.update(name, descriptor), repositoryUri, type);
+    return new KuduDataset<E>(namespace, name, kuduClient, kuduTable,
+        metadataProvider.update(name, descriptor), repositoryUri, type);
   }
 
   @Override
